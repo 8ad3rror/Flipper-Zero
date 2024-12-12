0c6dad0d247a27a6a994935a6dfd5e67149d7176
8ad3rror <zimny30c@gmail.com>
2024-12-12 22:44:09 +0100

    UP

--- BadUSB/Troll/TrolloWallpaper/AutoWall.bat
+++ BadUSB/Troll/TrolloWallpaper/AutoWall.bat
@@ -1,3 +1,10 @@
+:: Title: AutoWall
+:: Autor: 8ad3rror
+
 @echo off
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\AutoWall\trollo.jpg" /f
+set IMAGE_PATH=C:\AutoWall\trollo.jpg
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%IMAGE_PATH%" /f
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d "2" /f
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v TileWallpaper /t REG_SZ /d "0" /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+exit
